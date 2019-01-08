49ca3e3cf77b1bb54a42e58ef9c5d6b7cda04486
tryfor_ <619896855@qq.com>
2019-01-08 22:52:18 +0800

    test

--- dht/krpc.go
+++ dht/krpc.go
@@ -1,5 +1,6 @@
 package dht
 
+import "time"
 
 type KRPC struct {
 	ownNode *Node
@@ -7,5 +8,20 @@ type KRPC struct {
 }
 
 func  ParseBytesStream(data []byte) []*NodeInfo {
-	return nil
+	var nodes []*NodeInfo = nil
+	for j:=0; j<len(data); j = j+6 {
+		if j+26 > len(data) {
+			break
+		}
+		kn := data[j:j+26]
+		node := new(NodeInfo)
+		node.ID = Identifier(kn[0:20])
+		node.IP = kn[20:24]
+		port := kn[24:26]
+		node.Port = int(port[0])<<8 + int(port[1])
+		node.Status = GOOD
+		node.LastSeen = time.Now()
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
